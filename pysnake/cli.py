294a37d8da32e796b68d36fbc59dc2111e34d437
Carlos Porta <cmaciasporta@gmail.com>
2019-05-19 15:23:30 -0300

    cli shows points now

--- pysnake/cli.py
+++ pysnake/cli.py
@@ -2,8 +2,8 @@ from os import system, name
 
 from readchar import readchar
 
-from game import (change_snake_direction, Directions,
-                  next_state, random_state, State)
+from game import (change_snake_direction, snake_head, snake_tail,
+                  Directions, next_state, random_state, State)
 
 
 def _clear():
@@ -24,22 +24,33 @@ clear = _clear()
 
 def draw(state: State) -> None:
     clear()
+    print(f'Points: {state.points}/{(state.rows * state.cols) - 1}\n')
+    border = ''.join('/' for i in range(state.cols))
+    print(border)
     for i in range(state.rows):
         for j in range(state.cols):
-            if (i, j) in state.snake.body:
+            if (i, j) == snake_head(state):
+                print('@', end='')
+            elif (len(snake_tail(state)) > 0 and
+                 (i, j) == snake_tail(state)[-1]):
+                print('-', end='')
+            elif (i, j) in state.snake.body:
                 print('#', end='')
             elif (i, j) == state.food:
-                print('0', end='')
+                print('.', end='')
             else:
-                print('-', end='')
-        print()
+                print(' ', end='')
+        print('|')
+    print(border)
 
 
 def main():
-    state = random_state(2, 2)
+    rows = 2
+    cols = 3
+    state = random_state(rows, cols)
+    draw(state)
     key = ''
-    while state and key != 'q':
-        draw(state)
+    while state and key != 'q' and state.points < (rows * cols) - 1:
         key = readchar()
         if key == 'w':
             state = change_snake_direction(Directions.NORTH, state)
@@ -51,6 +62,8 @@ def main():
             state = change_snake_direction(Directions.WEST, state)
 
         state = next_state(state)
+        if state:
+            draw(state)
 
 
 if __name__ == "__main__":
